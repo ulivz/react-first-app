d5d2a19c10b5bceb525543f6ab2e55a4f354cc27
toxichl <472590061@qq.com>
2017-03-25 01:57:42 +0800

    Simple Update

---
+++ app/components/Video.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Video extends React.Component {
+
+	render() {
+		return (
+			<div>Video Component</div>
+		);
+
+	}
+
+}
+
+export {Video as default};
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,3 +1,14 @@
-import {Router} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
+import Shop from '../components/Shop';
+import Study from '../components/Study';
+import Video from '../components/Video';
 
-let ROUTER
+let ROUTER = (
+	<Router history={hashHistory}>
+		<Route path="/shop" component={Shop}/>
+		<Route path="/study" component={Study}/>
+		<Route path="/video" component={Video}/>
+	</Router>
+)
+
+export {ROUTER as default};
