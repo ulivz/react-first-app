a4a000bf947697ea31fe6d9d3b590ea36eec8260
toxichl <472590061@qq.com>
2017-03-24 22:40:13 +0800

    Simple Update

--- app/comment/CommentForm.js
+++ app/comment/CommentForm.js
@@ -7,9 +7,15 @@ class CommentList extends React.Component {
 	render() {
 		return (
 			<form className="ui reply form">
-				<div className="field"><input type="text"/></div>
-				<div className="filed textarea"></div>
-				<button type="submit"></button>
+				<div className="field">
+					<input type="text" placeholder="姓名"/>
+				</div>
+				<div className="filed textarea">
+					<textarea name="" placeholder="评论" id="" cols="30" rows="10"></textarea>
+				</div>
+				<button type="submit" className="ui blue button">
+					添加评论
+				</button>
 			</form>
 		)
 	}
