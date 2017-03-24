96fa37f3f0d2b2af048507f4140109952c89c4ac
toxichl <472590061@qq.com>
2017-03-25 01:51:31 +0800

    Simple Update

---
+++ app/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Shop extends React.Component{
+
+	render() {
+		return (
+			<div>Shop Component</div>
+		);
+
+	}
+
+
+}
---
+++ app/routes/routes.js
@@ -0,0 +1,3 @@
+import {Router} from 'react-router';
+
+let ROUTER
