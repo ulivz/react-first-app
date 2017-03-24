ad79e257a7487e06f3db256788580ffa12c972eb
toxichl <472590061@qq.com>
2017-03-24 23:48:37 +0800

    Simple Update

--- app/index.js
+++ app/index.js
@@ -6,10 +6,7 @@ import '../node_modules/semantic-ui/dist/semantic.min.css'
 import CommentBox from './comment/CommentBox'
 
 
-let comments = [
-	{"author": "Chen Haoli", "date": "5 minutes ago", "text": "What heavenly weather!"},
-	{"author": "Toxic HL", "date": "3 minutes ago", "text": "Let's go out!"}
-]
+
 
 // CommentBox
 // CommentList
@@ -17,6 +14,6 @@ let comments = [
 // CommentForm
 
 ReactDOM.render(
-  <CommentBox data={comments}/>,
+  <CommentBox url="app/comments.json"/>,
   document.getElementById('app')
 )
