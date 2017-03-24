a3fd6a1169fc967187199eaafc7fcf4443fc3194
toxichl <472590061@qq.com>
2017-03-24 18:11:39 +0800

    Simple Update

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		'webpack/hot/dev-server',
-		path.resolve(__dirname, './app/index.js')
-	],
+	entry: './app/index.js',
 	output: {
 		path: path.resolve(__dirname, './build'),
 		filename: 'bundle.js',
